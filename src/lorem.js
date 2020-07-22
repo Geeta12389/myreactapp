b87e554de4925348b0c279256aff0c1988aff785
Geeta12389 <68585814+Geeta12389@users.noreply.github.com>
2020-07-22 18:51:44 +0530

    lorem added

---
+++ src/lorem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Random images</h1>
+    <img src="https://picsum.photos/200" />
+  </div>,
+  document.getElementById("root")
+);
